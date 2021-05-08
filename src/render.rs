f096ffde68dc2e66f238111a795fe9f45e49bb4b
kaue <kaue.bazilli@usp.br>
2021-05-08 14:22:09 -0300

    Refactor renderer

--- src/render.rs
+++ src/render.rs
@@ -1,7 +1,14 @@
+use std::sync::{Arc, Mutex};
+use std::thread;
+use std::time::{Instant};
+
+use rayon::prelude::*;
+
 use sdl2::pixels::Color;
 
 use crate::env::Environment;
 
+#[derive(Clone)]
 pub struct Camera<T> where
     T: Environment
 {
@@ -29,3 +36,82 @@ impl<T> Camera<T> where
     }
 }
 
+pub struct Renderer<T> where
+    T: Environment
+{
+    screen: [u32; 2],
+    pub camera: Camera<T>,
+    pixels: Arc<Mutex<Vec<Color>>>,
+    running: Arc<Mutex<bool>>,
+    render_thread: Option<thread::JoinHandle<()>>,
+}
+
+impl<T> Renderer<T> where
+    T: Environment
+{
+    pub fn new(screen: [u32;2], camera: Camera<T>, pixels: Arc<Mutex<Vec<Color>>>, running: Arc<Mutex<bool>>) -> Renderer<T> {
+        let render_thread = None;
+        Renderer {screen, camera, pixels, running, render_thread}
+    }
+
+    pub fn start_render(&mut self) {
+        println!("Start render!");
+        self.stop_render();
+
+        let a_running = self.running.clone();
+        let a_pixels = self.pixels.clone();
+        let screen = self.screen.clone();
+        let camera = self.camera.clone();
+
+        self.render_thread = Some(thread::spawn(move || {
+            let t0 = Instant::now();
+            (0..screen[0]*screen[1]).into_par_iter().for_each(|ii| {
+                // Check if quit
+                {
+                    let running = a_running.lock().unwrap();
+                    if !*running {
+                        return;
+                    }
+                }
+
+                let i = ii / screen[0];
+                let j = ii % screen[0];
+                let pixel_color = camera.render_pixel(j, i, screen);
+                {
+                    let mut pixels = a_pixels.lock().unwrap();
+                    pixels[(i*screen[0] + j) as usize] = pixel_color;
+                }
+            });
+
+            {
+                let mut running = a_running.lock().unwrap();
+                *running = false;
+            }
+
+            println!("Finished rendering in {}s", t0.elapsed().as_nanos() as f64 / 1e9 );
+        }));
+
+        {
+            let mut running = self.running.lock().unwrap();
+            *running = true;
+        }
+    }
+
+    pub fn stop_render(&mut self) {
+        if let Some(join_handle) = self.render_thread.take() {
+            {
+                let mut running = self.running.lock().unwrap();
+                *running = false;
+            }
+            join_handle.join().unwrap();
+        }
+    }
+}
+
+impl<T> Drop for Renderer<T> where
+    T: Environment
+{
+    fn drop(&mut self) {
+        self.stop_render();
+    }
+}
